e2b1f779af08cbba7825b69b6f95bbd78c8a658e
Qwatrum <171450552+Qwatrum@users.noreply.github.com>
2024-07-25 21:54:57 +0200

    Added graph with matplotlib, showing the evolution

--- simulation.py
+++ simulation.py
@@ -1,14 +1,17 @@
 import random
+import matplotlib.pyplot as plt
+
 
 # i call the animals 'plics', i dont know why...
 
+round_saves = []
 plics = []
 cs = []
-AMOUNT_OF_PLICS = 10
-ODDS_OF_MUTATION = 100
+AMOUNT_OF_PLICS = 80
+ODDS_OF_MUTATION = 70
 AMOUNT_OF_KILLS_IN_PERCENT = 50  # max 50
 START_C = 0
-AMOUNT_OF_ROUNDS = 250
+AMOUNT_OF_ROUNDS = 1000
 
 
 class Plic():
@@ -49,15 +52,46 @@ def setup():
         cs.append(START_C)
 
 def start():
+    global round_saves
     setup()
+    round_saves.append(cs.copy())
     for i in range(AMOUNT_OF_ROUNDS):
+
         kill_visible_ones()
         reproduce()
         
+        round_saves.append(cs.copy())
+        
+
+    
+    
     print("Finished!")
     print(f"Highest c is {max(cs)}")
     print(f"Average is {sum(cs) / len(cs)}")
-    print(cs)
 
+    plot_data()
+    
+
+def plot_data():
+    max_data = []
+    avergae_data = []
+    
+    for i in round_saves:
+        
+        max_data.append(max(i))
+        avergae_data.append(sum(i) / len(i))
+    
+    plt.plot(max_data, "b-", label="Max")
+    plt.plot(avergae_data, "y-", label="Average")
+    plt.xlabel("Rounds / Generations")
+    plt.ylabel("Amount")
+    plt.legend(loc="upper left")
+    
+    plt.text(AMOUNT_OF_ROUNDS+2, max_data[-1], str(max_data[-1]))
+    plt.text(AMOUNT_OF_ROUNDS+2, avergae_data[-1], str(avergae_data[-1]))
+
+
+    plt.show()
+    
 
 start()
